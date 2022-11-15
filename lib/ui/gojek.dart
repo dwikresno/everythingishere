73729d86b1333c4e1c1fc3a1791258a57128c681
Dwi Kresno <dwikresno11@yahoo.co.id>
2022-11-15 17:51:40 +0700

    feat : fixing gojek and upload apk

--- lib/ui/gojek.dart
+++ lib/ui/gojek.dart
@@ -78,6 +78,9 @@ class _GojekState extends State<Gojek> {
           ),
           Column(
             children: [
+              SizedBox(
+                height: 20,
+              ),
               Container(
                 margin: EdgeInsets.symmetric(
                   vertical: 20,
