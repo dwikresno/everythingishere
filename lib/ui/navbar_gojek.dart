10abf31e2ef40dc155a719e777ffc5d3297bccf0
Dwi Kresno <dwikresno11@yahoo.co.id>
2022-11-25 21:16:34 +0700

    feat : move to bottom

--- lib/ui/navbar_gojek.dart
+++ lib/ui/navbar_gojek.dart
@@ -44,132 +44,128 @@ class _NavbarGojekState extends State<NavbarGojek> {
       body: Container(
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
-        child: Center(
-          child: Container(
-            padding: EdgeInsets.symmetric(horizontal: 10),
-            width: double.infinity,
-            height: 50,
-            decoration: BoxDecoration(
-              boxShadow: [
-                BoxShadow(
-                  color: Color(0xFFDBDBDB),
-                  blurRadius: 2.0,
-                  spreadRadius: 2.0,
-                ),
-              ],
-              color: Colors.white,
+      ),
+      bottomSheet: Container(
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        width: double.infinity,
+        height: 50,
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Color(0xFFDBDBDB),
+              blurRadius: 2.0,
+              spreadRadius: 2.0,
             ),
-            child: Stack(
-              alignment: Alignment.center,
-              children: [
-                AnimatedAlign(
-                  alignment: Alignment(posX!, -1),
-                  duration: Duration(milliseconds: 500),
-                  curve: Curves.linear,
-                  child: Column(
-                    children: [
-                      AnimatedContainer(
-                        width:
-                            MediaQuery.of(context).size.width / 4 - minusWidth,
-                        alignment: Alignment(posX!, -1),
-                        duration: Duration(milliseconds: 200),
-                        height: 4,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.only(
-                            bottomLeft: Radius.circular(4),
-                            bottomRight: Radius.circular(4),
-                          ),
-                          color: Color(0xff00880d),
-                        ),
+          ],
+          color: Colors.white,
+        ),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            AnimatedAlign(
+              alignment: Alignment(posX!, -1),
+              duration: Duration(milliseconds: 500),
+              curve: Curves.linear,
+              child: Column(
+                children: [
+                  AnimatedContainer(
+                    width: MediaQuery.of(context).size.width / 4 - minusWidth,
+                    alignment: Alignment(posX!, -1),
+                    duration: Duration(milliseconds: 200),
+                    height: 4,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(4),
+                        bottomRight: Radius.circular(4),
                       ),
-                      Visibility(
-                        visible: isShow,
-                        child: Container(
-                          width: MediaQuery.of(context).size.width / 4 -
-                              minusWidth,
-                          height: 40,
-                          decoration: BoxDecoration(
-                            gradient: LinearGradient(
-                              begin: Alignment.topCenter,
-                              end: Alignment.bottomCenter,
-                              colors: [
-                                Color(0xff00880d).withOpacity(0.1),
-                                Colors.transparent,
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
+                      color: Color(0xff00880d),
+                    ),
                   ),
-                  onEnd: () {
-                    Future.delayed(Duration(milliseconds: 200), () {
-                      setState(() {
-                        isShow = true;
-                        minusWidth = 10;
-                      });
-                    });
-                  },
+                  Visibility(
+                    visible: isShow,
+                    child: Container(
+                      width: MediaQuery.of(context).size.width / 4 - minusWidth,
+                      height: 40,
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          colors: [
+                            Color(0xff00880d).withOpacity(0.1),
+                            Colors.transparent,
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              onEnd: () {
+                Future.delayed(Duration(milliseconds: 200), () {
+                  setState(() {
+                    isShow = true;
+                    minusWidth = 10;
+                  });
+                });
+              },
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {
+                      movePage(0);
+                    },
+                    child: Container(
+                      color: Colors.transparent,
+                      child: Icon(
+                        Icons.home,
+                      ),
+                    ),
+                  ),
                 ),
-                Row(
-                  children: [
-                    Expanded(
-                      child: GestureDetector(
-                        onTap: () {
-                          movePage(0);
-                        },
-                        child: Container(
-                          color: Colors.transparent,
-                          child: Icon(
-                            Icons.home,
-                          ),
-                        ),
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {
+                      movePage(1);
+                    },
+                    child: Container(
+                      color: Colors.transparent,
+                      child: Icon(
+                        Icons.explore,
                       ),
                     ),
-                    Expanded(
-                      child: GestureDetector(
-                        onTap: () {
-                          movePage(1);
-                        },
-                        child: Container(
-                          color: Colors.transparent,
-                          child: Icon(
-                            Icons.explore,
-                          ),
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      child: GestureDetector(
-                        onTap: () {
-                          movePage(2);
-                        },
-                        child: Container(
-                          color: Colors.transparent,
-                          child: Icon(
-                            Icons.message,
-                          ),
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      child: GestureDetector(
-                        onTap: () {
-                          movePage(3);
-                        },
-                        child: Container(
-                          color: Colors.transparent,
-                          child: Icon(
-                            Icons.email,
-                          ),
-                        ),
-                      ),
-                    )
-                  ],
+                  ),
                 ),
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {
+                      movePage(2);
+                    },
+                    child: Container(
+                      color: Colors.transparent,
+                      child: Icon(
+                        Icons.message,
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {
+                      movePage(3);
+                    },
+                    child: Container(
+                      color: Colors.transparent,
+                      child: Icon(
+                        Icons.email,
+                      ),
+                    ),
+                  ),
+                )
               ],
             ),
-          ),
+          ],
         ),
       ),
     );
